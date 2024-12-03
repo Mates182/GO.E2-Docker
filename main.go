7be243610ed0c8560b427e3688dba99e69caea17
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-02 19:46:24 -0500

    feat: :sparkles: Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "Unknown"
+	}
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String()
+			}
+		}
+	}
+	return "Unknown"
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome, this page is deployed on Docker\n")
-	fmt.Fprintf(w, "By: Mateo Pillajo :D\n")
-	fmt.Fprintf(w, "Made with Golang\n")
+	localIP := getLocalIP()
+
+	fmt.Fprintf(w, "<h1>Welcome, this page is deployed on Docker</h1>")
+	fmt.Fprintf(w, "<h1>By: Mateo Pillajo :D</h1>")
+	fmt.Fprintf(w, "<h1>Local IP: %s</h1>", localIP)
 }
 func main() {
 	http.HandleFunc("/", handler)
